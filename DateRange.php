0d7b397974a7e0a0e275a1d38f01aadb4ce5f1c9
ChiperSoft <chiper@chipersoft.com>
2012-02-14 14:11:42 -0800

    DateRange - Presets rewritten to use new constructor

--- DateRange.php
+++ DateRange.php
@@ -33,45 +33,28 @@ class DateRange {
 	}
 	
 	static function ThisWeek() {
-		$o = new self();
-		$o->start = new DateTime('today -'.date('w').' days');
-		$o->end = new DateTime('today +'.(6-date('w')).' days');
-		return $o;
+		return new self('today -'.date('w').' days' , 'today +'.(6-date('w')).' days');
 	}
 	
 	static function LastWeek() {
-		$o = new self();
-		$o->start = new DateTime('today -'.(date('w')+7).' days');
-		$o->end = new DateTime('today -'.(date('w')+1).' days');
-		return $o;
+		return new self('today -'.(date('w')+7).' days' , 'today -'.(date('w')+1).' days');
 	}
 	
 	static function ThisMonth() {
-		$o = new self();
-		$o->start = new DateTime( date('m').'/01/'.date('Y') );
-		$o->end = new DateTime(date('m').'/01/'.date('Y'). '+1 month -1 day');
-		return $o;
+		return new self( date('m').'/01/'.date('Y') , date('m').'/01/'.date('Y'). '+1 month -1 day');
 	}
 	
 	static function LastMonth() {
-		$o = new self();
-		$o->start = new DateTime( (date('m')-1).'/01/'.date('Y') );
-		$o->end = new DateTime( (date('m')-1).'/01/'.date('Y'). '+1 month -1 day' );
-		return $o;
+		return new self( (date('m')-1).'/01/'.date('Y') , (date('m')-1).'/01/'.date('Y'). '+1 month -1 day' );
 	}
 
 	static function ThisYear() {
-		$o = new self();
-		$o->start = new DateTime( '01/01/'.date('Y') );
-		$o->end = new DateTime( '12/31/'.date('Y') );
-		return $o;
+		return new self( '01/01/'.date('Y') , '12/31/'.date('Y') );
 	}
 	
 	static function LastYear() {
-		$o = new self();
-		$o->start = new DateTime( '01/01/'.(date('Y')-1) );
-		$o->end = new DateTime( '12/31/'.(date('Y')-1) );
-		return $o;
+		return new self( '01/01/'.(date('Y')-1) , '12/31/'.(date('Y')-1) );
 	}
 	
 }
+
