1dfdef3ca9f75ad30c05723b3df698ee6c0ca60b
ChiperSoft <chiper@chipersoft.com>
2012-07-17 09:05:02 -0700

    File - Added tail($n) for getting the last $n lines of the file

--- File.php
+++ File.php
@@ -296,6 +296,32 @@ class File
 		return $c;
 	}
 	
+	public function tail($lines = 10) {
+        $data = '';
+        $fp = fopen($this->_file, "r");
+        $block = 4096;
+        $max = filesize($this->_file);
+
+        for($len = 0; $len < $max; $len += $block) {
+            $seekSize = ($max - $len > $block) ? $block : $max - $len;
+            fseek($fp, ($len + $seekSize) * -1, SEEK_END);
+            $data = fread($fp, $seekSize) . $data;
+
+            if(substr_count($data, "\n") >= $lines + 1) {
+                /* Make sure that the last line ends with a '\n' */
+                if(substr($data, strlen($data)-1, 1) !== "\n") {
+                    $data .= "\n";
+                }
+
+                preg_match("!(.*?\n){". $lines ."}$!", $data, $match);
+                fclose($fp);
+                return $match[0];
+            }
+        }
+        fclose($fp);
+        return $data; 
+    }
+	
 	/**
 	 * Returns a human readable file size in B/K/M/G/T
 	 * 
